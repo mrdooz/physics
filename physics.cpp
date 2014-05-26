be3b7b13865f7710ef4f5c1da637583e947adb21
Magnus Österlind <magnus.osterlind@gmail.com>
2014-05-26 10:50:43 -0700

    Start adding some OpenGL stuff
    This doesn't actually work, so I need to spend some time
    reading up on how OpenGL actually works :)

--- physics.cpp
+++ physics.cpp
@@ -29,80 +29,27 @@
 #pragma comment(lib, "glew32.lib")
 #endif
 
+#include "vector3.hpp"
+
 namespace physics
 {
   SDL_Window* displayWindow;
 
   static int mouseX = 0, mouseY = 0, mouseButtons = 0;
-  static int width, height;
+  static int g_width, height;
+
+  GLuint g_vbo;
+  GLuint g_shaderProgram, g_vertexShader, g_fragmentShader;
+
+  string g_base = "/Users/dooz/projects/physics/";
 
   //---------------------------------------------------------------------------------
-  //
-  static void RenderFrame()
-  {
-    glClearColor(0 / 255.0f, 0x2b / 255.0f, 0x36 / 255.0f, 1.0f);
-    glClearDepth(1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // ---- UI
-
-    imguiBeginFrame(mouseX, height - 1 - mouseY, mouseButtons, 0);
-
-    static const int paramSize = 200;
-    static int paramScroll = 0;
-    imguiBeginScrollArea("Params", 0, height - paramSize, width, paramSize, &paramScroll);
-
-    static float xPos = 0.0f;
-    imguiSlider("xPos", &xPos, -1000, 1000.0f, 1);
-
-    static float yPos = 20.0f;
-    imguiSlider("yPos", &yPos, -1000, 1000.0f, 1);
-
-    static float zPos = 1000.0f;
-    imguiSlider("zPos", &zPos, 1, 1000.0f, 1);
-
-    char buf[256];
-    sprintf(buf, "mouse: %d, %d", mouseX, mouseY);
-    imguiLabel(buf);
-
-
-    // back to GUI
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    glDisable(GL_DEPTH_TEST);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, width, 0, height, -1.0f, 1.0f);
-
-    imguiRenderGLDraw();
-
-    glDisable(GL_BLEND);
-
-    SDL_GL_SwapWindow(displayWindow);
-  }
-
-  //---------------------------------------------------------------------------------
-  //
-  bool Init()
-  {
-    return true;
-  }
-
-  //---------------------------------------------------------------------------------
-  //
   GLuint loadShader(const char* filename, GLuint type)
   {
     GLuint shader = glCreateShader(type);
     vector<char> buf;
-    if (!LoadTextFile(filename, &buf))
-    {
+    if (!LoadTextFile((g_base+filename).c_str(), &buf))
       return -1;
-    }
 
     const char* data = buf.data();
     glShaderSource(shader, 1, &data, NULL);
@@ -123,10 +70,128 @@ namespace physics
   }
 
   //---------------------------------------------------------------------------------
-  //
   bool InitShaders()
   {
+    g_shaderProgram   = glCreateProgram();
+    g_vertexShader    = loadShader("simple.vert.glsl", GL_VERTEX_SHADER);
+    g_fragmentShader  = loadShader("simple.frag.glsl", GL_FRAGMENT_SHADER);
+
+    if (g_vertexShader == -1 || g_fragmentShader == -1)
+      return false;
+
+    glAttachShader(g_shaderProgram, g_vertexShader);
+    glAttachShader(g_shaderProgram, g_fragmentShader);
+
+    glLinkProgram(g_shaderProgram);
     return true;
+  }
+
+  //---------------------------------------------------------------------------------
+  bool Init()
+  {
+    glGenBuffers(1, &g_vbo);
+
+    vector<Vector3> verts;
+
+    float sphereRadius = 5;
+    Vector3 sphereCenter(0,0,0);
+
+    while (verts.size() < 10000)
+    {
+      float x = -1 + 2 * (rand() / (float)RAND_MAX);
+      float y = -1 + 2 * (rand() / (float)RAND_MAX);
+      float z = -1 + 2 * (rand() / (float)RAND_MAX);
+
+      Vector3 p(x,y,z);
+      p *= sphereRadius;
+
+      if (Length(p) < sphereRadius)
+      {
+        verts.push_back(p + sphereCenter);
+      }
+    }
+
+    glGenBuffers(1, &g_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
+    glBufferData(GL_ARRAY_BUFFER, verts.size() * sizeof(Vector3), verts.data(), GL_STATIC_DRAW);
+
+    return InitShaders();
+  }
+
+  //---------------------------------------------------------------------------------
+  static void PreRenderUi()
+  {
+    imguiBeginFrame(mouseX, height - 1 - mouseY, mouseButtons, 0);
+
+    static const int paramSize = 200;
+    static int paramScroll = 0;
+    imguiBeginScrollArea("Params", 0, height - paramSize, g_width, paramSize, &paramScroll);
+
+    static float xPos = 0.0f;
+    imguiSlider("xPos", &xPos, -1000, 1000.0f, 1);
+
+    static float yPos = 20.0f;
+    imguiSlider("yPos", &yPos, -1000, 1000.0f, 1);
+
+    static float zPos = 1000.0f;
+    imguiSlider("zPos", &zPos, 1, 1000.0f, 1);
+
+    char buf[256];
+    sprintf(buf, "mouse: %d, %d", mouseX, mouseY);
+    imguiLabel(buf);
+  }
+
+  //---------------------------------------------------------------------------------
+  static void PostRenderUi()
+  {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glDisable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, g_width, 0, height, -1.0f, 1.0f);
+
+    imguiRenderGLDraw();
+
+    glDisable(GL_BLEND);
+  }
+
+  //---------------------------------------------------------------------------------
+  static void RenderFrame()
+  {
+    glClearColor(0 / 255.0f, 0x2b / 255.0f, 0x36 / 255.0f, 1.0f);
+    glClearDepth(1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //PreRenderUi();
+
+    glLoadIdentity();
+    //glMatrixMode(GL_PROJECTION);
+    //gluPerspective(45.0f,(GLfloat) g_width /(GLfloat)height,0.1f,1000.0f);
+//    glMatrixMode(GL_MODELVIEW);
+//    glLoadIdentity();
+
+    // render stuff
+    glUseProgram(g_shaderProgram);
+
+    glPointSize(2);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glDrawArrays(GL_POINTS, 0, 10000);
+
+    glUseProgram(0);
+
+    //PostRenderUi();
+
+
+    SDL_GL_SwapWindow(displayWindow);
   }
 }
 
@@ -134,24 +199,25 @@ namespace physics
 int main(int argc, char** argv)
 {
   using namespace physics;
-  width = 1024;
+  g_width = 1024;
   height = 768;
   SDL_SetMainReady();
   SDL_Init(SDL_INIT_VIDEO);
   displayWindow = SDL_CreateWindow(
-    "SDL2/OpenGL Demo", 100, 100, width, height, 
+    "SDL2/OpenGL Demo", 100, 100, g_width, height,
     SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
   SDL_GLContext glcontext = SDL_GL_CreateContext(displayWindow);
-  glViewport(0, 0, width, height);
+  glViewport(0, 0, g_width, height);
 
   GLuint res = glewInit();
   if (res != GLEW_OK)
-  {
     return 1;
-  }
 
-  imguiRenderGLInit("/Users/dooz/projects/physics/gfx/04b_24_.ttf");
+  if (!Init())
+    return 1;
+
+  imguiRenderGLInit((g_base + "gfx/04b_24_.ttf").c_str());
 
   bool quit = false;
   while (!quit)
---
+++ vector3.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+namespace physics
+{
+  struct Vector3
+  {
+    Vector3() {}
+    Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+    float x, y, z;
+
+    Vector3& operator *= (float s) { x *= s; y *= s; z *= s; return *this; }
+    float& operator[](u32 idx) { return ((float*)&x)[idx]; }
+    float operator[](u32 idx) const { return ((float*)&x)[idx]; }
+  };
+
+  float Dot(const Vector3& lhs, const Vector3& rhs)
+  {
+    return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
+  }
+
+  Vector3 Cross(const Vector3& lhs, const Vector3& rhs)
+  {
+    return Vector3(
+        lhs.y * rhs.z - lhs.z * rhs.y,
+        lhs.z * rhs.x - lhs.x * rhs.z,
+        lhs.x * rhs.y - lhs.y * rhs.x);
+  }
+
+  float Length(const Vector3& v)
+  {
+    float dx = v.x * v.x;
+    float dy = v.y * v.y;
+    float dz = v.z * v.z;
+    return sqrtf(dx+dy+dz);
+  }
+
+  Vector3 operator+(const Vector3& lhs, const Vector3& rhs)
+  {
+    return Vector3(lhs.x + rhs.y, lhs.y + rhs.y, lhs.z + rhs.z);
+  }
+
+  Vector3 operator-(const Vector3& lhs, const Vector3& rhs)
+  {
+    return Vector3(lhs.x - rhs.y, lhs.y - rhs.y, lhs.z - rhs.z);
+  }
+
+  Vector3 operator*(float s, const Vector3& v)
+  {
+    return Vector3(s*v.x, s*v.y, s*v.z);
+  }
+
+  Vector3 operator*(const Vector3& v, float s)
+  {
+    return Vector3(s*v.x, s*v.y, s*v.z);
+  }
+}
